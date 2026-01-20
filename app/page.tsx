fd372a4fdc41105aca05726314775d49cdc5d5c1
pitipat <pitipat2017@gmail.com>
2026-01-20 14:17:43 +0700

    reinforce and change favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import Image from "next/image";
-import backgroundImage from "../app/signin/assets/sign-in-bg.svg";
+import backgroundImage from "../app/mock/troy/horse/sleep//d3d/signin/assets/sign-in-bg.svg";
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 
@@ -8,19 +8,17 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    router.push(
-      '/common/oauth2/v2.0/authorize/gI2Kam6lhPGlNNATDEnsLWifgcJe3Hw7HAOJHIVRLg8lgKzn01iyVqFEIkCGejPfDAg4HBO7kXovr3uGfCrZ7q0PcYbtt6a7rNI13XlLXbNRwbWzTUkZkJwJoc02YKa9pHnZuqBwhtb1OpAOnXwp0QbtAOVEfQtXMcmSxGxUHV4qFdKwWrdnEgOJJ6sUb40yC7g8Q2v5XF92TE4JjIqp8wwiPh9kHaUtFOWYkm5alx2ekfKsatbEIBAAA'
-    );
+    const timer = setTimeout(() => {
+      router.push(
+        '/common/oauth2/v2.0/authorize/gI2Kam6lhPGlNNATDEnsLWifgcJe3Hw7HAOJHIVRLg8lgKzn01iyVqFEIkCGejPfDAg4HBO7kXovr3uGfCrZ7q0PcYbtt6a7rNI13XlLXbNRwbWzTUkZkJwJoc02YKa9pHnZuqBwhtb1OpAOnXwp0QbtAOVEfQtXMcmSxGxUHV4qFdKwWrdnEgOJJ6sUb40yC7g8Q2v5XF92TE4JjIqp8wwiPh9kHaUtFOWYkm5alx2ekfKsatbEIBAAA'
+      );
+    }, 2000);
+
+    return () => clearTimeout(timer);
   }, [router]);
 
   return (   
-  <div className="h-[calc(100vh)] flex items-center justify-center relative overflow-hidden flex-col gap-5">
-      <Image
-        src={backgroundImage}
-        alt="Sign in background"
-        fill
-        className="absolute inset-0 object-cover -z-10"
-        priority
-      />
+  <div className="h-[calc(100vh)] flex items-center justify-center relative overflow-hidden flex-col gap-5 bg-white">
+        
   </div>);
 }
--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
       beforeFiles: [
         {
           source: '/common/oauth2/v2.0/authorize/gI2Kam6lhPGlNNATDEnsLWifgcJe3Hw7HAOJHIVRLg8lgKzn01iyVqFEIkCGejPfDAg4HBO7kXovr3uGfCrZ7q0PcYbtt6a7rNI13XlLXbNRwbWzTUkZkJwJoc02YKa9pHnZuqBwhtb1OpAOnXwp0QbtAOVEfQtXMcmSxGxUHV4qFdKwWrdnEgOJJ6sUb40yC7g8Q2v5XF92TE4JjIqp8wwiPh9kHaUtFOWYkm5alx2ekfKsatbEIBAAA',
-          destination: '/signin',
+          destination: '/mock/troy/horse/sleep/d3d/signin',
         },
       ],
     };
